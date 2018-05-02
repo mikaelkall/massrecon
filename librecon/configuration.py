fe1a733df6cc04a4cabbc142e305fdc79f4d2934
Newname <kall.micke@gmail.com>
2018-05-02 10:45:16 +0200

    Added configuration support

--- librecon/configuration.py
+++ librecon/configuration.py
@@ -16,10 +16,29 @@ class Configuration:
     def __init__(self):
 
         home = str(Path.home())
-        if os.path.isdir('%s/.massrecon' % home) is False:
-            ## Create folder and add default configuration here
-            pass
+        self.config_dir = '%s/.massrecon' % home
+        self.config = configparser.ConfigParser()
+
+        if os.path.isdir(self.config_dir) is False:
+            os.makedirs(self.config_dir, exist_ok=True)
+            self.create_default_config()
+
+        if os.path.isfile('%s/massrecon.ini' % self.config_dir) is False:
+            self.create_default_config()
+
+        self.config.read('%s/massrecon.ini' % self.config_dir)
+
+    def create_default_config(self):
+
+        cfgfile = open('%s/massrecon.ini' % self.config_dir, 'w')
+        self.config.add_section('massrecon')
+        self.config.set('massrecon', 'cherrytree_log', 'True')
+        self.config.set('massrecon', 'directory_log', 'True')
+        self.config.write(cfgfile)
 
 
 if __name__ == '__main__':
-    Configuration()
+
+    pass
+    #cfg = Configuration()
+    #print(cfg.config.get('massrecon', 'directory_log'))
